d5cc234c1a774dd8a1d092c8e82cced26ad2359c
vivek <vivekgonbare666@gmail.com>
2025-06-20 17:29:07 +0530

    added scheduler to stop event streaming after a minuite

--- src/main/java/com/event_handler/event_handler_ms/publisher/WikiEventsProducer.java
+++ src/main/java/com/event_handler/event_handler_ms/publisher/WikiEventsProducer.java
@@ -3,33 +3,46 @@ package com.event_handler.event_handler_ms.publisher;
 
 import com.launchdarkly.eventsource.EventHandler;
 import com.launchdarkly.eventsource.EventSource;
-import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.net.URI;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 
 @Service
 public class WikiEventsProducer {
 
+    private static final Logger logger = LoggerFactory.getLogger(WikiEventsProducer.class);
 
+    @Value("${wikimedia.url}")
+    private String wikiMediaUrl;
+
+    @Value("${topic.wiki}")
+    private String wikiTopic;
     @Autowired
     private KafkaTemplate<String,String> kafkaTemplate;
 
 
+    public void sendWikiData() throws InterruptedException {
+        EventHandler eventHandler = new WikiEventHandler(kafkaTemplate,wikiTopic);
 
-    String url = "https://stream.wikimedia.org/v2/stream/recentchange";
-
-//    public void sendWikiData() throws InterruptedException {
-//        EventHandler eventHandler = new WikiEventHandler(kafkaTemplate,"wiki-topic");
-//
-//        EventSource eventSource = new EventSource.Builder(eventHandler, URI.create(url)).build();
-//        eventSource.start();
-//        TimeUnit.MINUTES.sleep(1);
-//    }
+        EventSource eventSource = new EventSource.Builder(eventHandler, URI.create(wikiMediaUrl)).build();
+        eventSource.start();
+        logger.info("Stream Started..!!!");
+        ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+        scheduledExecutorService.schedule(()-> {
+            logger.info("Stopping Stream after a minute...!!!!!");
+            eventSource.close();
+        },20,TimeUnit.SECONDS);
+        TimeUnit.MINUTES.sleep(1);
+    }
 
 
 

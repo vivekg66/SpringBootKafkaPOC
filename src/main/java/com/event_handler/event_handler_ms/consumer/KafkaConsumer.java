f7de809a75ed73610481ee3c9c8d1c771d692311
vivek <vivekgonbare666@gmail.com>
2025-06-20 17:47:17 +0530

    added consumer for wikimedia

--- src/main/java/com/event_handler/event_handler_ms/consumer/KafkaConsumer.java
+++ src/main/java/com/event_handler/event_handler_ms/consumer/KafkaConsumer.java
@@ -1,5 +1,7 @@
 package com.event_handler.event_handler_ms.consumer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -8,10 +10,19 @@ import java.util.Map;
 @Service
 public class KafkaConsumer {
 
+
+    private static final Logger logger = LoggerFactory.getLogger(KafkaConsumer.class);
+
     @KafkaListener(topics = "learn-topic",groupId = "myGroup")
     public void consumerEvents(Map<String, Object> eventMessage) {
-        System.out.println("Message received-->"+ eventMessage);
+        logger.info("Message received-->{}", eventMessage);
     }
+
+    @KafkaListener(topics = "wiki-topic",groupId = "wiki-group")
+    public void consumeWikiEvents(String wikiEvents) {
+        logger.info("WikiEvents-->{}",wikiEvents);
+    }
+
 
 
 }
